2663fdf3f7eb1dbf416403e1a6565dc495806521
Meredith Packham <m3r3dithe1ise@gmail.com>
2014-08-22 16:30:54 -0500

    added Data Manager class

---
+++ HomrMPRAC/DataManager.h
@@ -0,0 +1,13 @@
+//
+//  DataManager.h
+//  HomrMPRAC
+//
+//  Created by Meredith Packham on 8/22/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataManager : NSObject
+
+@end
---
+++ HomrMPRAC/DataManager.m
@@ -0,0 +1,13 @@
+//
+//  DataManager.m
+//  HomrMPRAC
+//
+//  Created by Meredith Packham on 8/22/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import "DataManager.h"
+
+@implementation DataManager
+
+@end
